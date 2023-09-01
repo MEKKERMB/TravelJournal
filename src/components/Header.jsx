5ca8cba62ce15148773c9aba6b44a18afde0f329
Mads Lundsby Bisgaard <mads@malbi.dk>
2023-09-01 15:12:58 +0200

    Create header (#1)
    * Create Header.jsx
    
    With the required styles
    
    * Update App.jsx
    
    Add Header element to the App

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="flex h-14 items-center justify-center bg-rose-500">
+      <h2 className="text-2xl text-white">my travel journal.</h2>
+    </header>
+  );
+};
+
+export default Header;
