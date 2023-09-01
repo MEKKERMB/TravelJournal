68e50a290f450fe71fbae1e3a04527d2b09f4745
Mads Lundsby Bisgaard <mads@malbi.dk>
2023-09-01 18:13:29 +0200

    Update App.jsx
    Map over data to generate the cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import Header from "./components/Header";
+import Card from "./components/Card";
+
+import cardData from "./data/card";
 
 function App() {
+  const cards = cardData.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
+
   return (
     <>
       <Header />
+      <section className="mx-auto flex max-w-6xl flex-col gap-10 p-20">
+        {cards}
+      </section>
     </>
   );
 }
